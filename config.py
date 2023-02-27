94b77d105e16c3e36a17ea9a4f88707c11e35bd0
taoidle <me@mail.taoidle.com>
2023-02-28 01:55:12 +0800

    [add] config

---
+++ config.py
@@ -0,0 +1,7 @@
+NETWORK_CONFIG_PATH = "/etc/owl/net-server/wifi_list.json"
+
+OTA_RECEIVE_DIR = "./files/"
+OTA_SERVICE_LIST = ["owl", "apriltag", "owl-net-timer.timer"]
+OTA_PUB_KEY = "./.ssh/id_rsa.pub"
+OTA_TARGET_FILENAME = "ota_update.zip"
+OTA_UPDATE_DIR = './ota_files/'
